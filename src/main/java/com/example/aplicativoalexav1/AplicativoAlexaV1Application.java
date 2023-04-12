0859e197eb8e35f7c6778f4946052d393613ec75
Lucas Silva Carvalho <politicolc@MacBook-Pro-de-Lucas.local>
2023-04-11 22:43:01 -0300

    Initial commit

---
+++ src/main/java/com/example/aplicativoalexav1/AplicativoAlexaV1Application.java
@@ -0,0 +1,13 @@
+package com.example.aplicativoalexav1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicativoAlexaV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AplicativoAlexaV1Application.class, args);
+    }
+
+}
